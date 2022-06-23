73b8047ddb8302bef33d5195f2ec69e194070536
Xavier Valois-Smith <96591678+Code-Xavier@users.noreply.github.com>
2022-06-23 16:53:24 -0700

    "the moment of truth"

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,2 +1,159 @@
+//idea: populate the scene with a bunch of musical intruments instead of stars
+
 import './style.css'
+
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Loader } from 'three';
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+document.getElementsByClassName('icon')[0].addEventListener('click', myFunction);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// const renderer = new THREE.WebGLRenderer({
+//   canvas: document.querySelector('#bg')
+// });
+
+const canvas = document.querySelector('#bg');
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  alpha: true,
+  premultipliedAlpha: false,
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30)
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const loader = new THREE.TextureLoader();
+const material = new THREE.MeshStandardMaterial({ map: loader.load('/images/t.jpg'), });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.set(25, 25, 25);
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+scene.add(pointLight, ambientLight);
+
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color:0xFFFFFF });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+Array(200).fill().forEach(addStar);
+
+
+
+// const spaceTexture = new THREE.TextureLoader();
+// // scene.background = spaceTexture.load( 'space.jpg' );
+// scene.background = spaceTexture.load( 'https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' );
+
+//Avatar 
+
+const xavierTexture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/245px-Unofficial_JavaScript_logo_2.svg.png');
+
+const xavier = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial( {map: xavierTexture  })
+); 
+
+scene.add(xavier);
+
+//moon
+
+// const moonTexture = new THREE.TextureLoader().load('https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+// // const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+// const moon = new THREE.Mesh(
+//   new THREE.SphereGeometry(3, 32, 32),
+//   new THREE.MeshStandardMaterial( 
+//     { map: moonTexture, 
+//       // normalMap: normalTexture
+    
+//     })
+// );
+
+// scene.add(moon);
+
+// moon.position.z = 30;
+// moon.position.setX(-10);
+
+
+//move camera
+
+function moveCamera() {
+
+  const t = document.body.getBoundingClientRect().top;
+  // moon.rotation.x += 0.05;
+  // moon.rotation.y += 0.075;
+  // moon.rotation.z += 0.05;
+
+  xavier.rotation.y += 0.01;
+  xavier.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.y = t * -0.0002;
+  camera.position.x = t * -0.0002;
+
+}
+
+document.body.onscroll = moveCamera;
+
+window.addEventListener( 'resize', onWindowResize, false );
+			
+function onWindowResize() {
+
+   camera.aspect = window.innerWidth / window.innerHeight;
+   camera.updateProjectionMatrix();
+   renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+
+  renderer.render(scene, camera);
+}
+
+animate()
+
+
 
